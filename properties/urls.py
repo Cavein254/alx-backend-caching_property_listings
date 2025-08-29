09ae0548df72dbba72b664cff652fec63bc7b25a
johnmuthua <caveomuthama@gmail.com>
2025-08-29 12:54:32 +0300

    feat: add cached views

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PropertyListCreateView
+from .views import PropertyListCreateView, property_list
 
 urlpatterns = [
-    path('properties/', PropertyListCreateView.as_view(), name='property-list-create'),
+    path('properties/', property_list, name='property-list'),
+    path('properties-crud/', PropertyListCreateView.as_view(), name='property-list-create'),
+
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Property
 from .serializers import PropertySerializer
 from .utils import get_all_properties
@@ -15,3 +19,10 @@ class PropertyListCreateView(generics.ListCreateAPIView):
         from django.core.cache import cache
         cache.delete("all_properties")
         return instance
+
+@api_view(["GET"])
+@cache_page(60 * 15)  # cache for 15 minutes
+def property_list(request):
+    properties = Property.objects.all().order_by("-created_at")
+    serializer = PropertySerializer(properties, many=True)
+    return Response(serializer.data)
